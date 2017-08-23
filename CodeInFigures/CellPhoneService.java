48e32734aa1fd762b261c148fba53f96bc1fa396
Jordan <jordan.togo@my.jcu.edu.au>
2017-08-23 14:48:30 +1000

    fix layout for cellphone exercise

--- CodeInFigures/CellPhoneService.java
+++ CodeInFigures/CellPhoneService.java
@@ -5,15 +5,24 @@ public class CellPhoneService {
         int talk;
         int text;
         int data;
+        int price;
         final int PLANA = 49;
         final int PLANB = 55;
         final int PLANC = 61;
         final int PLAND = 70;
         final int PLANE = 79;
         final int PLANF = 87;
-        String input;
-        input = JOptionPane.showInputDialog(null, "Enter minutes needed");
-        talk = Integer.parseInt(input);
+        final int TALKMIN = 500;
+        final int TEXTMIN = 100;
+        final int DATAMIN = 2;
+        String input, output, plan;
 
+        input = JOptionPane.showInputDialog(null, "Enter amount minutes needed");
+        talk = Integer.parseInt(input);
+        input = JOptionPane.showInputDialog(null, "Enter amount of messages needed");
+        text = Integer.parseInt(input);
+        input = JOptionPane.showInputDialog(null, "Enter amount of data needed");
+        data = Integer.parseInt(input);
+        
     }
 }
