f24eb162597709e93eb076832ff5954bde815e8a
Jordan <jordan.togo@my.jcu.edu.au>
2017-08-30 11:30:00 +1000

    complete cellphine exercise

--- CodeInFigures/CellPhoneService.java
+++ CodeInFigures/CellPhoneService.java
@@ -6,16 +6,16 @@ public class CellPhoneService {
         int text;
         int data;
         int price;
-        final int PLANA = 49;
-        final int PLANB = 55;
-        final int PLANC = 61;
-        final int PLAND = 70;
-        final int PLANE = 79;
-        final int PLANF = 87;
+        final int PRICEA = 49;
+        final int PRICEB = 55;
+        final int PRICEC = 61;
+        final int PRICED = 70;
+        final int PRICEE = 79;
+        final int PRICEF = 87;
         final int TALKMIN = 500;
         final int TEXTMIN = 100;
         final int DATAMIN = 2;
-        String input, output, plan;
+        String input, message, plan;
 
         input = JOptionPane.showInputDialog(null, "Enter amount minutes needed");
         talk = Integer.parseInt(input);
@@ -23,6 +23,41 @@ public class CellPhoneService {
         text = Integer.parseInt(input);
         input = JOptionPane.showInputDialog(null, "Enter amount of data needed");
         data = Integer.parseInt(input);
-        
+        if(data > 0)
+            if(data <= DATAMIN)
+            {
+                plan = "E";
+                price = PRICEE;
+            }
+            else
+            {
+                plan = "F";
+                price = PRICEF;
+            }
+        else
+        if(talk < TALKMIN)
+            if(text == 0 && data == 0)
+            {
+                plan = "A";
+                price = PRICEA;
+            }
+            else
+            {
+                plan = "B";
+                price = PRICEB;
+            }
+        else
+        if(text < TEXTMIN)
+        {
+            plan = "C";
+            price = PRICEC;
+        }
+        else
+        {
+            plan = "D";
+            price = PRICED ;
+        }
+        message = "Plan " + plan + "  $" + price + " per month";
+        JOptionPane.showMessageDialog(null, message);
     }
 }
